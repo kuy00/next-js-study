62cad946f6aa752149e64676765238b072bfc8cd
kuy <kuy472013@gmail.com>
2025-03-05 01:02:06 +0900

    슬롯 추가

---
+++ src/app/user/@slot/page.tsx
@@ -0,0 +1,7 @@
+import Text from "@/components/atoms/Text";
+
+const Slot = () => {
+  return <Text>Slot</Text>;
+};
+
+export default Slot;
--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,12 +1,15 @@
 export default function UserLayout({
   children,
   modal,
+  slot,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
+  slot: React.ReactNode;
 }) {
   return (
     <div>
+      {slot}
       {children}
       {modal}
     </div>
